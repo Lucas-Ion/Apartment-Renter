19d8fa4e3bace355075d6ee379511b8c93aa234f
Amnah Hussain <73035499+Amnah736@users.noreply.github.com>
2021-12-08 20:50:30 -0700

    Updated SQL Tables

--- propertySystem.sql
+++ propertySystem.sql
@@ -1,13 +1,5 @@
-DROP DATABASE IF EXISTS propertySystem;
 CREATE DATABASE propertySystem;
 USE propertySystem;
-
-DROP TABLE IF EXISTS landlordInfo;
-CREATE TABLE landlordInfo(
-	username varchar(255),
-    balance integer,
-    primary key(username)
-);
 
 DROP TABLE IF EXISTS propertyInfo;
 CREATE TABLE propertyInfo(
@@ -17,14 +9,12 @@ CREATE TABLE propertyInfo(
     numBed integer,
     numBath integer,
     isFurnished varchar(255),
-    address varchar(255),
     cityQuadrant char(2),
+    address varchar(255),
     landlordUsername varchar(255),
     primary key (propID),
     foreign key (landlordUsername) references landlordInfo(username) ON UPDATE CASCADE
 );
-
-
 
 DROP TABLE IF EXISTS userInfo;
 CREATE TABLE userInfo(
@@ -35,9 +25,21 @@ CREATE TABLE userInfo(
     phoneNo varchar(255),
     address varchar(255),
     userRole varchar(255),
-    notification varchar(255),
     userPassword varchar(255),
     primary key (username)
+);
+
+DROP TABLE IF EXISTS listingInfo;
+CREATE TABLE listingInfo(
+	renterUserName varchar(255),
+    foreign key (renterUserName) references renterInfo(username) ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS landlordInfo;
+CREATE TABLE landlordInfo(
+	username varchar(255),
+    balance integer,
+    primary key(username)
 );
 
 DROP TABLE IF EXISTS renterInfo;
@@ -45,12 +47,6 @@ CREATE TABLE renterInfo(
 	username varchar(255),
     searchCrit varchar(255),
     primary key(username)
-);
-
-DROP TABLE IF EXISTS listingInfo;
-CREATE TABLE listingInfo(
-	renterUserName varchar(255),
-    foreign key (renterUserName) references renterInfo(username) ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS managerInfo;
@@ -67,29 +63,14 @@ CREATE TABLE emailMessage(
     message varchar(255),
     primary key(messageID)
 );
-    
-Insert into userInfo
-values("username1", "hy", "huynh", 23, "phone1", "address1", "manager", "", "password1" );
 
-Insert into userInfo
-values("username2", "hy2", "huynh2", 13, "phone2", "address2", "renter", "", "password2" );
-
-Insert into userInfo
-values("username3", "hy3", "huynh3", 43, "phone3", "address3", "landlord", "", "password3" );
-
-Insert into userInfo
-values("username4", "hy4", "huynh4", 21, "phone4", "address4", "renter", "", "password4" );
-
-select * from userinfo;
-
-insert into landlordinfo
-values("username3", 5);
-
-Insert into propertyinfo
-values(1, "RENTED", "Apartment", 2, 3, true, "Nowhere", "NE", "username3" );
-
-select * from userinfo where (userRole="manager" and age=23);
-
+DROP TABLE IF EXISTS fees;
+CREATE TABLE fees(
+	feeID integer,
+	fee integer,
+    period integer,
+    primary key(feeID)
+);
 
 
 
